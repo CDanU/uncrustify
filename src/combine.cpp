CDanU/uncrustify

commit 42b1ed31999d4926188acf881912b49958282dac

Add sp_inside_oc_at_sel_parens and sp_after_oc_at_sel_parens. Make some @selector options also apply to @protocol(). Set the PCF_IN_OC_MSG flag instead of changing the parent of everything inside a OC msg.

src/combine.cpp
@@ -3886,11 +3886,13 @@ static void handle_oc_message_send(chunk_t *os)
    }
 
    os->parent_type = CT_OC_MSG;
+   os->flags      |= PCF_IN_OC_MSG;
    cs->parent_type = CT_OC_MSG;
+   cs->flags      |= PCF_IN_OC_MSG;
 
    for (tmp = chunk_get_next(os); tmp != cs; tmp = chunk_get_next(tmp))
    {
-      tmp->parent_type = CT_OC_MSG;
+      tmp->flags |= PCF_IN_OC_MSG;
       if (tmp->type == CT_COLON)
       {
          tmp->type = CT_OC_COLON;

src/space.cpp
@@ -840,9 +840,30 @@ argval_t do_space(chunk_t *first, chunk_t *second, bool complete = true)
          log_rule("sp_after_oc_type");
          return(cpd.settings[UO_sp_after_oc_type].a);
       }
+      else if (first->parent_type == CT_OC_SEL)
+      {
+         log_rule("sp_after_oc_at_sel_parens");
+         return(cpd.settings[UO_sp_after_oc_at_sel_parens].a);
+      }
    }
 
-   if ((first->type == CT_OC_SEL) && (second->type == CT_PAREN_OPEN))
+   if (cpd.settings[UO_sp_inside_oc_at_sel_parens].a != AV_IGNORE)
+   {
+      if (((first->type == CT_PAREN_OPEN) &&
+           ((first->parent_type == CT_OC_SEL) ||
+            (first->parent_type == CT_OC_PROTOCOL)))
+          ||
+          ((second->type == CT_PAREN_CLOSE) &&
+           ((second->parent_type == CT_OC_SEL) ||
+            (second->parent_type == CT_OC_PROTOCOL))))
+      {
+         log_rule("sp_inside_oc_at_sel_parens");
+         return(cpd.settings[UO_sp_inside_oc_at_sel_parens].a);
+      }
+   }
+
+   if ((second->type == CT_PAREN_OPEN) &&
+       ((first->type == CT_OC_SEL) || (first->type == CT_OC_PROTOCOL)))
    {
       log_rule("sp_after_oc_at_sel");
       return(cpd.settings[UO_sp_after_oc_at_sel].a);

src/options.cpp
@@ -407,7 +407,15 @@ void register_options(void)
                   "'-(int) f:(int)x;' vs '-(int)f:(int)x;'");
    unc_add_option("sp_after_oc_at_sel", UO_sp_after_oc_at_sel, AT_IARF,
                   "Add or remove space between '@selector' and '('\n"
-                  "'@selector(msgName).' vs '@selector (msgName)'");
+                  "'@selector(msgName)' vs '@selector (msgName)'\n"
+                  "Also applies to @protocol() constructs");
+   unc_add_option("sp_after_oc_at_sel_parens", UO_sp_after_oc_at_sel_parens, AT_IARF,
+                  "Add or remove space between '@selector(x)' and the following word\n"
+                  "'@selector(foo) a:' vs '@selector(foo)a:'");
+   unc_add_option("sp_inside_oc_at_sel_parens", UO_sp_inside_oc_at_sel_parens, AT_IARF,
+                  "Add or remove space inside '@selector' parens\n"
+                  "'@selector(foo)' vs '@selector( foo )'\n"
+                  "Also applies to @protocol() constructs");
    unc_add_option("sp_before_oc_block_caret", UO_sp_before_oc_block_caret, AT_IARF,
                   "Add or remove space before a block pointer caret\n"
                   "'^int (int arg){...}' vs. ' ^int (int arg){...}'");

src/options.h
@@ -305,6 +305,8 @@ enum uncrustify_options
    UO_sp_after_oc_type,
    UO_sp_after_oc_return_type,
    UO_sp_after_oc_at_sel,
+   UO_sp_after_oc_at_sel_parens,
+   UO_sp_inside_oc_at_sel_parens,
    UO_sp_before_oc_block_caret,
    UO_sp_after_oc_block_caret,
    UO_sp_cond_colon,

src/uncrustify_types.h
@@ -103,6 +103,7 @@ struct parse_frame
 #define PCF_IN_CLASS           PCF_BIT(10)
 #define PCF_IN_NAMESPACE       PCF_BIT(11)
 #define PCF_IN_FOR             PCF_BIT(12)
+#define PCF_IN_OC_MSG          PCF_BIT(13)
 
 /* Non-Copy flags are in the upper 48 bits */
 #define PCF_FORCE_SPACE        PCF_BIT(16)  /* must have a space after this token */
@@ -144,7 +145,7 @@ static const char *pcf_names[] =
    "IN_CLASS",          // 10
    "IN_NAMESPACE",      // 11
    "IN_FOR",            // 12
-   "#13",               // 13
+   "IN_OC_MSG",         // 13
    "#14",               // 14
    "#15",               // 15
    "FORCE_SPACE",       // 16

tests/output/oc/50008-protocol.m
@@ -1,7 +1,7 @@
 
-NSAssert([self.delegate conformsToProtocol: @protocol (UISearchBarDelegate)], @"Some Error.");
+NSAssert([self.delegate conformsToProtocol: @protocol(UISearchBarDelegate)], @"Some Error.");
 
-Protocol *counter = @protocol (ReferenceCounting);
+Protocol *counter = @protocol(ReferenceCounting);
 
 @protocol ReferenceCounting
 
@@ -19,7 +19,7 @@ Protocol *counter = @protocol (ReferenceCounting);
 
 @end
 
-if ([receiver conformsTo: @protocol (ReferenceCounting)])
+if ([receiver conformsTo: @protocol(ReferenceCounting)])
 {
    [receiver incrementCount];
 }

tests/output/oc/50050-real_world_file.m
@@ -183,7 +183,7 @@
                                                  name:NSWindowWillCloseNotification
                                                object:_window];
 
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowShouldClose:)name:NSApplicationDidResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowShouldClose:) name:NSApplicationDidResignActiveNotification object:nil];
 
     // Start watching events to figure out when to close the window
     NSAssert(_eventMonitor == nil, @"_eventMonitor should not be created yet");

commit 99a74db110453272bfe08f098d82b310e25a6c43

Fix detection of whether inside Obj-C msg send
Fixes sp_before_send_oc_colon and sp_after_send_oc_colon that were probabably broken by 42b1ed31999d4926188acf881912b49958282dac. Also revives the intent of cb9c866934ff2937e59eebab7dfeb937de73f792 broken by the same commit.

Signed-off-by: Hannes Schmidt <git3@hannesschmidt.net>

src/combine.cpp
@@ -666,7 +666,7 @@ void do_symbol_check(chunk_t *prev, chunk_t *pc, chunk_t *next)
        */
       if ((pc->type == CT_PAREN_OPEN) &&
           ((pc->parent_type == CT_NONE) ||
-           (pc->parent_type == CT_OC_MSG) ||
+           (pc->parent_type == CT_OC_MSG) || // TODO: use pc->flags & PCF_IN_OC_MSG instead?
            (pc->parent_type == CT_OC_BLOCK_EXPR)) &&
           ((next->type == CT_WORD) ||
            (next->type == CT_TYPE) ||
@@ -1749,7 +1749,7 @@ void combine_labels(void)
    /* unlikely that the file will start with a label... */
    while (next != NULL)
    {
-      if ((next->parent_type != CT_OC_MSG) && /* filter OC case of [self class] msg send */
+      if ((0 == next->flags & PCF_IN_OC_MSG) && /* filter OC case of [self class] msg send */
           ((next->type == CT_CLASS) ||
            (next->type == CT_OC_CLASS) ||
            (next->type == CT_TEMPLATE)))

src/space.cpp
@@ -661,7 +661,7 @@ argval_t do_space(chunk_t *first, chunk_t *second, bool complete = true)
       log_rule("sp_func_class_paren");
       return(cpd.settings[UO_sp_func_class_paren].a);
    }
-   if ((first->type == CT_CLASS) && (first->parent_type != CT_OC_MSG))
+   if ((first->type == CT_CLASS) && (0 == first->flags & PCF_IN_OC_MSG))
    {
       log_rule("FORCE");
       return(AV_FORCE);
@@ -1247,7 +1247,7 @@ argval_t do_space(chunk_t *first, chunk_t *second, bool complete = true)
    }
    if (first->type == CT_OC_COLON)
    {
-      if (first->parent_type == CT_OC_MSG)
+      if (first->flags & PCF_IN_OC_MSG)
       {
          log_rule("sp_after_send_oc_colon");
          return(cpd.settings[UO_sp_after_send_oc_colon].a);
@@ -1260,7 +1260,7 @@ argval_t do_space(chunk_t *first, chunk_t *second, bool complete = true)
    }
    if (second->type == CT_OC_COLON)
    {
-      if ((first->parent_type == CT_OC_MSG) &&
+      if ((first->flags & PCF_IN_OC_MSG) &&
           ((first->type == CT_OC_MSG_FUNC) ||
            (first->type == CT_OC_MSG_NAME)))
       {

src/token_enum.h
@@ -211,10 +211,10 @@ typedef enum
    CT_OC_CLASS_EXT,     /* ObjC: a pair of empty parens after the class name in a @interface or @implementation */
    CT_OC_CATEGORY,      /* ObjC: the category name in parens after the class name in a @interface or @implementation */
    CT_OC_SCOPE,         /* ObjC: the '-' or '+' in '-(void) func: (int) i;' */
-   CT_OC_MSG,           /* ObjC: parent type to '[class func : param name: param];' stuff */
-   CT_OC_MSG_CLASS,     /* ObjC: 'class' in  '[class func : param name: param];' */
-   CT_OC_MSG_FUNC,      /* ObjC: 'func' in  '[class func : param name: param];' */
-   CT_OC_MSG_NAME,      /* ObjC: 'name' in  '[class func : param name: param];' */
+   CT_OC_MSG,           /* ObjC: parent type to '[', ']' and ';' in '[class func : param name: param];' */
+   CT_OC_MSG_CLASS,     /* ObjC: 'class' in  '[class func : param name: param];' (see also PCF_IN_OC_MSG) */
+   CT_OC_MSG_FUNC,      /* ObjC: 'func' in  '[class func : param name: param];' (see also PCF_IN_OC_MSG) */
+   CT_OC_MSG_NAME,      /* ObjC: 'name' in  '[class func : param name: param];' (see also PCF_IN_OC_MSG) */
    CT_OC_MSG_SPEC,      /* ObjC: msg spec '-(void) func: (int) i;' */
    CT_OC_MSG_DECL,      /* ObjC: msg decl '-(void) func: (int) i { }' */
    CT_OC_RTYPE,         /* ObjC: marks closing parens of the return type after scope */

src/token_names.h
@@ -1,5 +1,5 @@
 /*
- * Generated by make_token_names.sh on Mon Nov 15 17:04:46 CST 2010
+ * Generated by make_token_names.sh on Sat Dec  4 15:17:11 PST 2010
  */
 #ifndef TOKEN_NAMES_H_INCLUDED
 #define TOKEN_NAMES_H_INCLUDED