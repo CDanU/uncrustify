c3c77a7f78bfb058f2eab1bf91a29c994c0fcef6
Daniel Chumak <chumak.daniel.dev@gmail.com>
2017-06-30 00:11:26 +0200

    test

---
+++ tests/output/cpp/34192-code_width.cpp
@@ -0,0 +1,195 @@
+class z
+{
+	void ff0(const string &s, string bbbbbb, 
+		 f(int i, int i, int i, int i, int i, int i, int i, int b),
+		 int iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii);
+
+	void ff1(const string &s, string bbbbbb, 
+		 f(int iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii, int i, int i, int b),
+		 int iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii);
+
+	void l0(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa, const string& xxxxxxxxxxxxx);
+	void l1(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		const string& xxxxxxxxxxxxxx);
+	void l2(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		const string& xxxxxxxxxxxxxxx);
+	void l3(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		const string& xxxxxxxxxxxxxxxx);
+
+	void f0(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		const string& xxxxxxxxxxxxx);
+	void f1(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		const string& xxxxxxxxxxxxxx);
+
+	void ll0(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl0(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll1(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl1(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll2(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl2(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll3(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl3(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll4(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl4(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll5(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl5(
+		const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll6(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl6(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+
+	void ll7(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+		 const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx);
+	void fl7(const string& xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+		 int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+}
+
+void main()
+{
+	auto abc = call_some_other_really_long_function.of_some_sort(
+		some_long_parameter1, some_long_parameter2);
+
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccccccccccccccccccccccccccccccccccccccccxxxxxxxxxxxxxxxxxcx");
+
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxccxxxxcxxxxxxxxxx",
+	  a);
+
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", f(),
+	  a);
+
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  f(), a);
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccxxxxxxxxx",
+	  f(), a);
+
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a, f());
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a, f());
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a, f());
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a, f());
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  a, f());
+	f("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccxxxxxxxxxxx",
+	  a, f());
+
+	f(a, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccxxxxxxxxxxxxxcccx");
+
+	f(f(), a, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(f(), a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(f(), a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(f(), a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(f(), a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(f(), a,
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccxxxxxxxxxxxcccx");
+
+	f(a, f(), "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+	f(a, f(), "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", d);
+	f(a, f(), "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  d);
+	f(a, f(), "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  d);
+	f(a, f(), "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  d);
+	f(a, f(), "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  d);
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcccx", d);
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxccxxx", d);
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxccxxxx", d);
+	f(a, f(),
+	  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxccxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	  d);
+
+	ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(a);
+	fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+		a);
+	ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+		a);
+	fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+		a);
+	ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+		a);
+	fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+		a);
+}
+
+virtual void fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+	int a);
+virtual void ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff(
+	int a);
