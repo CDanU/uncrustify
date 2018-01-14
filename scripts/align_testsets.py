a39e1935bee696ce285c6f098d7b6b24fe0e92ae
Daniel Chumak <chumak.daniel.dev@gmail.com>
2018-01-14 18:14:13 +0100

    tmp

--- scripts/align_testsets.py
+++ scripts/align_testsets.py
@@ -1,82 +1,324 @@
 from __future__ import print_function  # python >= 2.6
-from os.path import dirname, abspath, join
+from os.path import dirname, abspath, join, isfile, isdir
 from sys import exit
 from os import chdir
 from glob import glob
-from sys import stderr
+from sys import stderr, exit
+from tempfile import NamedTemporaryFile
 from re import match, compile as re_compile, X as re_X
+from shutil import copyfile
+from argparse import ArgumentParser
+
 
 ROOT_DIR = dirname(dirname(abspath(__file__)))
 TEST_DIR = join(ROOT_DIR, 'tests')
-COLS = [1, 8, 45, 77]
+CELL_COLS = [1, 8, 45, 77]  # cell col positions
+MIN_SPACE = 2               # min. padding for every cell
+# ------------------------------------------------------------------------------
 
+# stores the distance between cells the cells, one extra dummy elem for loops
+CELL_WIDTHS = [(CELL_COLS[idx] - CELL_COLS[idx-1])
+               for idx in range(1, len(CELL_COLS))] + [0]
+
+# regex used to extract information in an Uncrustify test line
 LINE_REG = re_compile(r"""
     ^
-    (\d+)          #1: test nr
-    \s*
-    (!?)           #2: rerun symbol
-    \s+
-    (\S+\.cfg)     #3: config file name
-    \s+
-    ((\w+)/(\S+))  #4: partial source file path, #5: test dir name, #6: source file name
-    \s*
-    (\S+)?         #7: language (optional)
-    (.*)           #8: remaining stuff (comments, etc.)
+    (\d+)              #1: test nr
+    (!?)[ ]+           #2: rerun symbol
+    (\S+\.cfg)[ ]+     #3: config file name
+    ((\w+)/(\S+))[ ]*  #4: source file path, #5: test dir name, #6: source file name
+    (\S+)?             #7: language (optional)
+    (.*)               #8: remaining stuff (comments, etc.)
     $
     """, re_X)
 
 
-def min_ljust(str, width, min=0, fillchar=None):
+def min_ljust(str, width, min=MIN_SPACE):
+    """
+    like ljust() but with a guaranteed minimum amount of padding added
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param str: str
+        string that is going to be adjusted
+    :param width: int
+        string length in total after padding
+    :param min: int
+        minimal amount of padding that will be applied
+
+
+    :return: str
+    ----------------------------------------------------------------------------
+        left padded string
+    """
+
     o_len = len(str)
-    if o_len > width:
+    if o_len > width - min:
         return str + ' ' * min
     else:
         return str.ljust(width)
 
 
-def process_line(line):
+def process_line(line, check_fcn=None):
+    """
+    applies parsing regex on a line, extracts needed regex groups and combines
+    them to the desired format
+
+    accesses global var(s): MIN_SPACE, LINE_REG, CELL_WIDTHS
+    depends upon a specific format of the LINE_REG
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param line: str
+        a single line that is going to be processed
+
+    :param check_fcn: function
+        an optional check function that is executed after the regex extraction
+        was performed
+
+
+    :return: str
+    ----------------------------------------------------------------------------
+        the formatted line
+    """
+
     line_s = line.strip()
 
     if not line_s or line_s[0] == '#':
-        return line  # ignore blank/comment lines
+        return line_s  # ignore blank/comment lines
 
     reg_obj = match(LINE_REG, line_s)
     if not reg_obj:
-        print("Warning: line does not match the regex:\n   "+line, file=stderr)
-        return line  # regex did not match
+        print("  Error: regex does not match:\n     "+line, file=stderr)
+        return None
+    if check_fcn and not check_fcn(reg_obj.regs):
+        print("  Error: checks failed:\n     "+line, file=stderr)
+        return None
 
-    test_nr = reg_obj.group(1)
-    rerun_symbol = reg_obj.group(2) or " "
-    cfg_name = reg_obj.group(3)
-    src_path = reg_obj.group(4)
-    lang = reg_obj.group(7) or ""
-    rest = reg_obj.group(8) or ""
+    reg_groups = [
+        reg_obj.group(1) + (reg_obj.group(2) or " "),  # testNr + rerunSymbol
+        reg_obj.group(3),  # cfgName
+        reg_obj.group(4),  # src_path
+        ((reg_obj.group(7) or "") + (" " * MIN_SPACE)
+         + (reg_obj.group(8) or "").strip()).strip(),  # lang + rest
+    ]
 
-    col0 = min_ljust(test_nr + rerun_symbol, COLS[1]-COLS[0], min=1)
-    col1 = min_ljust(cfg_name, COLS[2]-COLS[1], min=2)
-    col2 = min_ljust(src_path, COLS[3]-COLS[2], min=2)
-    col3 = lang
+    sub = 0
+    cell_txts = []
+    min = 1
 
-    line_s = (col0+col1+col2+col3+rest).strip()
+    for idx, (grp_str) in enumerate(reg_groups):
+        cell_txts.append(min_ljust(grp_str, CELL_WIDTHS[idx] - sub, min))
+        min = 2
+        sub = len(cell_txts[-1]) - CELL_WIDTHS[idx]
+
+    line_s = "".join(cell_txts).strip()
     return line_s
 
 
-def main():
-    chdir(TEST_DIR)
+def col_pos_check(reg_regs_obj):
+    """
+    checks the correctness of a positions list of groups of a regex object
 
+    accesses global var(s): CELL_COLS
+    depends upon a specific format of the LINE_REG
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param reg_regs_obj: list<pair<uint, uint>>
+        regex.regs object containing start and end pos of groups
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True - all checks pass, False otherwise
+    """
+
+    test_nr_start = reg_regs_obj[1][0] + 1
+    cfg_name_start = reg_regs_obj[3][0] + 1
+    src_path_start = reg_regs_obj[4][0] + 1
+    lang_start = reg_regs_obj[7][0] + 1
+
+    test_nr_end = reg_regs_obj[1][1] + 1
+    cfg_name_end = reg_regs_obj[3][1] + 1
+    src_path_end = reg_regs_obj[4][1] + 1
+
+    if test_nr_start != CELL_COLS[0]:
+        return False
+
+    if (cfg_name_start != CELL_COLS[1]
+        and test_nr_end < CELL_COLS[1] - MIN_SPACE)                            \
+       or abs(cfg_name_start - test_nr_end) < MIN_SPACE:
+            return False
+
+    if (src_path_start != CELL_COLS[2]
+        and cfg_name_end < CELL_COLS[2] - MIN_SPACE)                           \
+       or abs(src_path_start - cfg_name_end) < MIN_SPACE:
+            return False
+
+    if lang_start != 0:  # -1 + 1
+        if (lang_start != CELL_COLS[3]
+            and src_path_end < CELL_COLS[3] - MIN_SPACE)                       \
+           or abs(lang_start - src_path_end) < MIN_SPACE:
+            return False
+
+    return True
+
+
+def get_lines(file):
+    """
+    returns a list with all lines of a file (without newline character)
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file: str
+        file that is going to be read
+
+
+    :return: list<str>
+    ----------------------------------------------------------------------------
+        list with all lines of the file
+    """
+
+    with open(file, 'r') as f:
+        tmp_lines = f.read()
+        return tmp_lines.splitlines()
+
+
+def do_check(file_path):
+    """
+    checks if formatting is necessary
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file_path: str
+        file that is going  to be checked
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no formatting is needed, False otherwise
+    """
+
+    flag_ok = True
+
+    lines = get_lines(file_path)
+    for line in lines:
+        if process_line(line, col_pos_check) is None:
+            flag_ok = False
+
+    if not flag_ok:
+        print("Formatting error(s) in file: " + file_path, file=stderr)
+
+    return flag_ok
+
+
+def do_format(file_path):
+    """
+    applies the formatting
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file_path: str
+        file that is going to be formatted
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no errors occurred, False otherwise
+    """
+
+    flag_ok = True
+    lines = get_lines(file_path)
+
+    with NamedTemporaryFile('w') as f:
+        for line in lines:
+            line = process_line(line)
+
+            if line is None:
+                flag_ok = False
+                break
+
+            print(line, file=f)
+        f.flush()
+
+        if flag_ok:
+            copyfile(f.name, file_path)
+
+    return flag_ok
+
+
+def process_tests(dir, mode_fcn):
+    """
+    globs *.test files in a directory and applies the provided function on them
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param dir: str
+        path to a directory that includes *.test files that are going to be
+        processed
+
+    :param mode_fcn: function
+        function that is going to be applied on every file
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no errors occurred, False otherwise
+    """
+
+    chdir(dir)
+
+    flag_ok = True
     test_files = glob("*.test")
     lines = []
 
     for file in test_files:
-        with open(file, 'r') as f:
-            tmp_lines = f.read()
-            lines = tmp_lines.splitlines()
+        if not mode_fcn(file):
+            flag_ok = False
 
-        with open(file, 'w') as f:
-            for line in lines:
-                line = process_line(line)
-                print(line, file=f)
+    return flag_ok
 
 
 if __name__ == "__main__":
-    exit(main())
+    """
+    parses all script arguments and based on that calls needed functions
+    """
+
+    arg_parser = ArgumentParser()
+    group_mode = arg_parser.add_mutually_exclusive_group(required=True)
+    group_mode.add_argument(
+        '-c', '--check', action='store_true',
+        help="checks if formatting changes are necessary (1) or not (0)"
+    )
+    group_mode.add_argument('-f', '--format', action='store_true',
+                            help="applies formatting")
+    arg_parser.add_argument(
+        'input', metavar='INPUT', type=str,
+        help='either a single file or all *.test files inside a directory'
+    )
+
+    FLAGS, unparsed = arg_parser.parse_known_args()
+
+    mode_fcn = do_check if FLAGS.check else do_format
+
+    if isfile(FLAGS.input):
+        if not mode_fcn(FLAGS.input):
+            exit(1)
+    elif isdir(FLAGS.input):
+        if not process_tests(FLAGS.input, mode_fcn):
+            exit(1)
+    else:
+        print("Error: input is neither a file nor a directory\n  %s"
+              % FLAGS.input, file=stderr)
+        exit(1)
+
+    exit(0)
+
--- scripts/check_single_option_configs.py
+++ scripts/check_single_option_configs.py
@@ -1,14 +1,21 @@
-
 from __future__ import print_function  # python >= 2.6
-from os.path import dirname, abspath, join, split
+from os import rename as file_rename, remove as file_remove
+from os.path import join, isfile, isdir, basename, split as path_split
 from sys import exit
-from os import chdir
 from glob import glob
 from sys import stderr
+from argparse import ArgumentParser
+from re import match, compile as re_compile, X as re_X
+from tempfile import NamedTemporaryFile
+from shutil import copyfile
 
-ROOT_DIR = dirname(dirname(abspath(__file__)))
-CONFIG_DIR = join(ROOT_DIR, './tests/config')
+# ROOT_DIR = dirname(dirname(abspath(__file__)))
+# CONFIG_DIR = join(ROOT_DIR, './tests/config')
 
+# min. padding for every cell in a test config
+MIN_SPACE = 2  # keep in sync with align_testsets.py
+
+# maps option values to abbreviations using in the naming convention
 VALUE_MAP = {
     "true": "t",
     "false": "f",
@@ -29,19 +36,60 @@ VALUE_MAP = {
 }
 
 
+def is_comment_line(line):
+    """
+    checks if a string matches the following pattern: ^ *#
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param line: str
+        string on which the match is performed
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        true if line matches, False otherwise
+    """
+    first_non_sp_idx = next(idx for idx, char in enumerate(line) if char != ' ')
+    return True if(line[first_non_sp_idx] == '#') else False
+
+
+def do_skip_line(line):
+    """ returns True if line can be skipped (is empty or a comment line) """
+    return True if (not line or is_comment_line(line)) else False
+
+
 def parse_config_line(line):
-    if not line:
-        return None, None        # return on empty string
+    """
+    parses a Uncrustify config line to extract option and value
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param line: str
+        string that is going to be parsed
+
+
+    :return: str, str
+    ----------------------------------------------------------------------------
+        config name and config value or "","" if skippable line
+        or None, None in case of failure
+    """
+
+    if do_skip_line(line):
+        return None, None        # return on empty/comment string
 
     pound_pos = line.find('#')
     if pound_pos != -1:
-        if pound_pos == 0:
-            return None, None    # comment line -> empty string
-        line = line[:pound_pos]  # cut away the  comment part
+        line = line[:pound_pos]  # cut away the comment part
 
     split_pos = line.find('=')
     if split_pos == -1:
-        split_pos = line.find(' ')
+        first_non_sp_idx = next(idx for idx, char in enumerate(line)
+                                if char != ' ')
+        split_pos = line.find(' ', first_non_sp_idx)
+
     if split_pos == -1:
         return None, None        # no appropriate separator found
 
@@ -50,72 +98,377 @@ def parse_config_line(line):
     return key, value
 
 
-def get_change_set(file_paths):
-    lines = []
-    options = {}
+def get_change_set_file(file_path):
+    """
+    checks if a config file is a single option config line and if it needs
+    to be renamed to follow the naming convention
 
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file_path: str
+        path to to file that is going to be tested
+
+
+    :return: dict<str, str>
+    ----------------------------------------------------------------------------
+        single element dict, key = old file path, value = new file path,
+        empty dict if no renaming can or needs to be done
+    """
+
+    options = {}
     change_set = {}
 
-    for file_path in file_paths:
-        options.clear()
+    lines = get_lines(file_path)
+    for idx, (line) in enumerate(lines):
+        if do_skip_line(line):
+            continue
 
-        with open(file_path, 'r') as f:
-            tmp_lines = f.read()
-            lines = tmp_lines.splitlines()
+        key, value = parse_config_line(line)
 
-        for idx, (line) in enumerate(lines):
-            if not line or line[0] == '#':
-                continue  # skip empty lines
-            key, value = parse_config_line(line)
+        if not key or not value:
+            raise ValueError("error while parsing line %d, %s"
+                             % (idx, file_path))
+        if key in options:
+            raise RuntimeError("option defined multiple times: %s" % key)
 
-            if key and value:
-                if key in options:
-                    raise RuntimeError("option '%s' defined multiple times"
-                                       % key)
-                options[key] = value
+        options[key] = value
+        if len(options) > 1:
+            break       # multi option file -> ignore this file
 
-                if len(options) > 1:
-                    break  # multi option file -> ignore this file
-            else:
-                raise ValueError("error while parsing line %d, %s"
-                                 % (idx, file_path))
+    if not options or len(options) > 1:
+        return {}       # no/multi option file -> ignore this file
 
-        if not options or len(options) > 1:
-            continue  # multi option file -> ignore
+    key, value = options.popitem()
 
-        key, value = options.popitem()
+    if value in VALUE_MAP:
+        value = VALUE_MAP[value]
+    else:
+        try:
+            int(value)  # if number use the number string
+        except ValueError:
+            # else: some other string -> ignore this file
+            # => no naming convention for AT_STRING options
+            return {}
 
-        if value in VALUE_MAP:
-            value = VALUE_MAP[value]
-        else:
-            try:
-                int(value)  # if number use the number string
-            except ValueError:
-                continue    # else: some other string -> ignore this file
+    expected_name = key + "-" + value + ".cfg"
 
-        expected_name = key+"-"+value+".cfg"
+    f_path, f_name = path_split(file_path)
 
-        f_path, f_name = split(file_path)
-
-        if f_name != expected_name:
-            change_set[file_path] =  join(f_path, expected_name)
-            pass
+    if f_name != expected_name:
+        change_set[file_path] = join(f_path, expected_name)
 
     return change_set
 
 
-def main():
-    chdir(CONFIG_DIR)
-    test_files = glob("*.cfg")
+def get_change_set(file_paths):
+    """
+    like get_change_set_file, but for multiple files, returns multi element dict
+    """
 
-    change_set = get_change_set(test_files)
+    change_set = {}
+
+    for file_path in file_paths:
+        cs = get_change_set_file(file_path)
+        change_set.update(cs)
+
+    return change_set
+
+
+def dir_glob(dir_path, glob_str):
+    """
+    like glob.glob but returns paths based on (containing) the provided dir_path
+    """
+
+    files = glob(join(dir_path, glob_str))
+    return files
+
+
+def change_set_check(file_paths):
+    """
+    calls get_change_set, prints messages to stderr if renamings need to be done
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file_paths: list<str>
+        path to to file(s) that are going to be tested
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no renaming of files needs to be done, False otherwise
+    """
+
+    change_set = get_change_set(file_paths)
 
     if change_set:
+        print("File(s) that need to be renamed:", file=stderr)
         for k, v in change_set.items():
-            print("Error: file '%s' needs to be renamed to '%s'"
-                  % (k, v), file=stderr)
-        return 1
+            print("  '%s' --> '%s'" % (basename(k), basename(v)), file=stderr)
+
+        return False
+    return True
+
+
+def do_check_dir(dir_path):
+    """ globs dir_path for cfg files and returns value from change_set_check """
+    test_files = dir_glob(dir_path, "*.cfg")
+    return change_set_check(test_files)
+
+
+def do_check_file(file_path):
+    """ calls change_set_check with a single file """
+    return change_set_check([file_path])
+
+
+def get_lines(file):
+    """
+    returns a list with all lines of a file (without newline character)
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file: str
+        file that is going to be read
+
+
+    :return: list<str>
+    ----------------------------------------------------------------------------
+        list with all lines of the file
+    """
+
+    with open(file, 'r') as f:
+        tmp_lines = f.read()
+        return tmp_lines.splitlines()
+
+
+def rename_in_test(test_file_path, change_set_names):
+    """
+    applies renamings inside test files
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param test_file_path:
+        path to a test file
+
+    :param change_set_names: dict<str, str>
+        dict containing old (key) and new (value) config names (not paths!)
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no errors occurred, False otherwise
+    """
+
+    line_regex = re_compile(r"""
+        ^\d+!?[ ]+         # test nr + rerun symbol
+        (\S+\.cfg)([ ]+)   #1: config file name #2: spaces
+        """, re_X)
+
+    lines_i = get_lines(test_file_path)
+
+    with NamedTemporaryFile('w') as f:
+        flag_ok = True
+
+        for line in lines_i:
+            if do_skip_line(line):
+                print(line, file=f)
+                continue
+
+            # apply regex to get cfg file name & amount of spaces after it
+            reg_obj = match(line_regex, line)
+            if not reg_obj:
+                print("  Error: regex does not match:\n     " + line,
+                      file=stderr)
+                return False
+
+            cfg_name = reg_obj.group(1)
+            len_spaces = len(reg_obj.group(2))
+
+            # do the replacement, preserve alignment if possible
+            if cfg_name in change_set_names:
+                replacement = change_set_names[cfg_name]
+
+                len_delta = len(cfg_name) - len(replacement)
+                spaces = " " * max(MIN_SPACE, len_spaces + len_delta)
+
+                line = line[0:reg_obj.regs[1][0]] + replacement + spaces \
+                       + line[reg_obj.regs[2][1]:]
+
+            print(line, file=f)
+
+        f.flush()
+
+        if flag_ok:
+            copyfile(f.name, test_file_path)
+
+    return flag_ok
+
+
+def rename_in_tests(change_set_names, test_dir):
+    """ applies rename_in_test, for all *.test files inside test_dir """
+    files = dir_glob(test_dir, "*.test")
+    for file in files:
+        if not rename_in_test(file, change_set_names):
+            return False
+
+    return True
+
+
+def check_file_rename(old_path, new_path):
+    """
+    renames/moves config file,
+
+    if new_path file exists:
+        - checks if it is a single option file and if it does have the same
+          option as the old_path single option file,
+        - appends comment lines of old_path into new_path
+        - delletes old_path, keeps new_path
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param old_path: str
+        path to the file that is going to be renamed/moved
+
+    :param new_path: str
+        target path where old_path file is going to be moved to
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no errors occurred, False otherwise
+    """
+
+    # if file exists already
+    if isfile(new_path):
+        # use nameing convention of the file to get name and shortened value
+        new_name = basename(new_path)
+        o_name, o_val = new_name.rsplit("-", 1)
+
+        # (reverse ambiguous naming: force, false -> f -> force||false)
+        # check only the first char, should be good enough
+        o_val = o_val.rsplit(".", 1)[0][0]
+
+        # check if it is has only the according (one) option and value
+        with open(new_path, 'r') as f:
+            l_nr = 0
+            for line in f:
+                l_nr += 1
+                if do_skip_line(line):
+                    continue
+
+                k, v = parse_config_line(line)
+                if not k or not v:
+                    print("error while parsing line %d\n %s" % (l_nr, new_path),
+                          file=stderr)
+                    return False
+                if k != o_name or v[0] != o_val:
+                    print("Option/Value combination missing: %s = %s...\n"
+                          "or not a single option file:\n %s" % old_path,
+                          file=stderr)
+                    return False
+
+        # check if the old file has comment lines
+        comment_lines = []
+        with open(old_path, 'r') as f:
+            for line in f:
+                if is_comment_line(line):
+                    comment_lines.append(line.strip())
+        # append them to the existing new_file
+        if comment_lines:
+            with open(new_path, 'a') as f:
+                print("\n", file=f)         # print 2 newlines
+                for line in comment_lines:
+                    print(line, file=f)
+
+        file_remove(old_path)
+        return True
+
+    file_rename(old_path, new_path)
+    return True
+
+
+def change_set_apply(file_paths, test_dir):
+    """
+    applies renamings if needed (in actual config files and inside test files)
+
+
+    Parameters
+    ----------------------------------------------------------------------------
+    :param file_paths:
+        paths to the files that are going to be processed
+
+    :param test_dir:
+        path to the directory containing test files
+
+
+    :return: bool
+    ----------------------------------------------------------------------------
+        True if no errors occurred, False otherwise
+    """
+    change_set = get_change_set(file_paths)
+    if not change_set:
+        return True
+
+    # test files only use the file name, remove the path
+    change_set_names = {}
+    for k, v in change_set.items():
+        k = basename(k)
+        v = basename(v)
+        change_set_names[k] = v
+    if not rename_in_tests(change_set_names, test_dir):
+        print("Error while renaming option(s) in tests file(s)", file=stderr)
+
+    for k, v in change_set.items():
+        if not check_file_rename(k, v):
+            return False
+    return True
+
+
+def do_apply_file(file_path, test_dir):
+    """ calls change_set_apply with a single config file path """
+    return change_set_apply([file_path], test_dir)
+
+
+def do_apply_dir(dir_path, test_dir):
+    """ calls change_set_apply with a all config file paths inside test_dir """
+    file_paths = dir_glob(dir_path, "*.cfg")
+    return change_set_apply(file_paths, test_dir)
 
 
 if __name__ == "__main__":
-    exit(main())
+    """
+    parses all script arguments and based on that calls needed functions
+    """
+
+    arg_parser = ArgumentParser()
+    group_mode = arg_parser.add_mutually_exclusive_group(required=True)
+    group_mode.add_argument(
+        '-c', '--check', action='store_true',
+        help="checks if renaming changes are necessary (1) or not (0)"
+    )
+
+    group_mode.add_argument('-a', '--apply', action='store_true',
+                            help="applies renaming")
+    arg_parser.add_argument(
+        '-t', '--testsdir', type=str, required='--apply',
+        help="directory containing *.test files in which the renaming also "
+             "will be done"
+    )
+
+    arg_parser.add_argument(
+        'input', metavar='INPUT', type=str,
+        help='either a single file or all *.cfg files inside a directory'
+    )
+
+    FLAGS, unparsed = arg_parser.parse_known_args()
+
+    if FLAGS.apply:
+        mode_fcn = do_apply_dir if isdir(FLAGS.input) else do_apply_file
+        exit(0 if mode_fcn(FLAGS.input, FLAGS.testsdir) else 1)
+
+    mode_fcn = do_check_dir if isdir(FLAGS.input) else do_check_file
+    exit(0 if mode_fcn(FLAGS.input) else 1)
